103ece0af75230d632a10b138602b028a842e791
Trịnh Nguyễn Hoàng Vũ <trinhnguyenhoangvu306@gmail.com>
2024-04-25 19:17:12 +0700

    modified templates and test views

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+import sqlite3
+import pandas as pd
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    conn = sqlite3.connect('database.db')
+    query = f"SELECT DISTINCT ID FROM StockData;"
+    df = pd.read_sql_query(query, conn)
+    return render_template('home.html', df=df)
